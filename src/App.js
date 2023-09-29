4a34b097952927a6a8dd306a2b206d4f8010fd4b
Gaurav Roy <96660449+gauravxroy@users.noreply.github.com>
2023-09-29 15:29:34 +0530

    app.js added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-// 
-import './App.css';
+//
+import "./App.css";
 import Aos from "aos";
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
 // import Footer from './components/Footer/Footer';
-import { useEffect } from 'react';
-import Hero from './components/UI/Hero';
-import Projects from './components/UI/Projects';
-import Contact from './components/UI/Contact';
-import Footer from './components/Footer/Footer';
-
+import { useEffect } from "react";
+import Hero from "./components/UI/Hero";
+import Projects from "./components/UI/Projects";
+import Contact from "./components/UI/Contact";
+import Footer from "./components/Footer/Footer";
+import About from "./components/UI/About";
 
 function App() {
-  useEffect(()=>{
+  useEffect(() => {
     Aos.init();
-  },[])
-  return ( 
+  }, []);
+  return (
     <>
-    <Header/>
-   <main>
-    <Hero/>
-    <Projects/>
-    <Contact/>
-   </main>
-   <Footer/>
-   </>
+      <Header />
+      <main>
+        <Hero />
+        <Projects />
+        <About />
+        <Contact />
+      </main>
+      <Footer />
+    </>
   );
 }
 
