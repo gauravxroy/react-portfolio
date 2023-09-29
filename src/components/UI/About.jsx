aeb2f605593d2c25c470e257d17c012c01b27d7a
Gaurav Roy <96660449+gauravxroy@users.noreply.github.com>
2023-09-29 17:08:05 +0530

    new changes

--- src/components/UI/About.jsx
+++ src/components/UI/About.jsx
@@ -4,19 +4,19 @@ const About = () => {
   return (
     <section id="about" className=" bg-[#1f2937] ">
       <div
-        class="max-w-sm w-full lg:max-w-full lg:flex container"
+        className="max-w-sm w-full lg:max-w-full lg:flex container"
         data-aos="zoom-in-up"
         data-aos-duration="1200"
       >
         {/* <div
-          class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+          className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
           title="Woman holding a mug"
         ></div> */}
-        <div class="border-solid border-2  lg:border-gray-400 bg-white rounded-xl p-4 flex flex-col justify-between leading-normal">
-          <div class="mb-8">
-            <p class="text-sm text-gray-600 flex items-center">
+        <div className="border-solid border-2  lg:border-gray-400 bg-white rounded-xl p-4 flex flex-col justify-between leading-normal">
+          <div className="mb-8">
+            <p className="text-sm text-gray-600 flex items-center">
               <svg
-                class="fill-current text-gray-500 w-3 h-3 mr-2"
+                className="fill-current text-gray-500 w-3 h-3 mr-2"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
@@ -24,10 +24,10 @@ const About = () => {
               </svg>
               Members only
             </p>
-            <div class="text-gray-900 font-bold text-xl mb-2">
+            <div className="text-gray-900 font-bold text-xl mb-2">
               Can coffee make you a better developer?
             </div>
-            <p class="text-gray-700 text-base">
+            <p className="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
               exercitationem praesentium nihil. Lorem ipsum dolor sit amet
@@ -38,15 +38,15 @@ const About = () => {
               aspernatur!
             </p>
           </div>
-          <div class="flex items-center">
+          <div className="flex items-center">
             <img
-              class="w-10 h-10 rounded-full mr-4"
+              className="w-10 h-10 rounded-full mr-4"
               src={AvatarImg}
               alt="Avatar of Gaurav"
             />
-            <div class="text-sm">
-              <p class="text-gray-900 leading-none">Gaurav Roy</p>
-              <p class="text-gray-600">Software Developer</p>
+            <div className="text-sm">
+              <p className="text-gray-900 leading-none">Gaurav Roy</p>
+              <p className="text-gray-600">Software Developer</p>
             </div>
           </div>
         </div>
