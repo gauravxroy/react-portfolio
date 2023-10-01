2ea7793d2dd3d78e85205b164afe84198f9fcfbe
Gaurav Roy <96660449+gauravxroy@users.noreply.github.com>
2023-10-02 00:16:38 +0530

    new project card changes added

--- src/components/UI/Projects.jsx
+++ src/components/UI/Projects.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import cardImg from "../Asset/Images/cable.jpg";
+
+import projectImg1 from "../Asset/Images/HomePage.png";
+import { FaGithub, FaLink } from "react-icons/fa";
 const Projects = () => {
   return (
-    <section id="projects" className="bg-[#1f2937]">
+    <section id="projects" className="bg-[#111827]">
       <div className="container ">
         <div className="mb-10 flex items-center justify-between flex-wrap">
           <div className="mb-7  sm:mb-0">
@@ -11,54 +13,67 @@ const Projects = () => {
             </h2>
           </div>
         </div>
-        <div className=" flex items-center justify-center  gap-5 flex-wrap cursor-pointer">
+        <div className=" flex items-center justify-center  gap-10 flex-wrap ">
           {/* 1st card start */}
-          <div
-            className="cardshadow w-full bg-white p-4  border-solid border-2  lg:w-1/4 rounded-xl hover:opacity-90 hover:scale-105 transform transition duration-500 hover:shadow-md "
-            data-aos="zoom-in-up"
-            data-aos-duration="1500"
-          >
-            <div>
-              <img className=" rounded-xl  " src={cardImg} alt="" />
-            </div>
-            <div>
-              <h2 className="pt-4 text-violet-500"> Full Stack Site </h2>
-              <p className="text-gray-700">
-                Web Application For Watching movies Developed using MERN Stack{" "}
-              </p>
-              <button className="border-[1.5px] rounded-xl max-h-10 px-4 p-2 mt-4  text-white  font-600 bg-violet-500 border-solid border-violet-500  hover:bg-white hover:text-violet-500 hover:font-[500] ease-in duration-300">
-                View{" "}
+          <div class="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+            <div class="relative mx-4 mt-4 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
+              <img src={projectImg1} alt="project img 01" />
+              <div class="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60"></div>
+              <button
+                class="!absolute top-4 right-4 h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-red-500 transition-all hover:bg-red-500/10 active:bg-red-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button"
+                data-ripple-dark="true"
+              >
+                <span class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
+                  <a href="/">
+                    {" "}
+                    <FaLink size={20} />
+                  </a>
+                </span>
               </button>
+            </div>
+            <div class="p-6">
+              <div class="mb-3 flex items-center justify-between">
+                <h5 class="block font-sans text-xl font-medium leading-snug tracking-normal text-blue-gray-900 antialiased">
+                  MoviZZ
+                </h5>
+                <span class="flex items-center gap-2  text-base font-normal leading-relaxed text-blue-gray-900 antialiased">
+                  <a href="/">
+                    {" "}
+                    <FaGithub size={20} />
+                  </a>
+                  <a href="/">
+                    {" "}
+                    <FaLink size={20} />
+                  </a>
+                </span>
+              </div>
+              <p class="block font-sans text-base font-light leading-relaxed text-gray-700 antialiased">
+                Enter a freshly updated and thoughtfully furnished peaceful home
+                surrounded by ancient trees, stone walls, and open meadows.
+              </p>
+              n
+            </div>
+            <div class="p-6 pt-3">
+              <a href="/">
+                <button
+                  class="block w-full select-none rounded-lg bg-pink-500 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  type="button"
+                  data-ripple-light="true"
+                >
+                  View
+                </button>
+              </a>
             </div>
           </div>
           {/* 1st card end */}
 
           {/* 2nd card start */}
-          <div
-            className="cardshadow w-full p-4 bg-white border-solid border-2  lg:w-1/4 rounded-xl hover:opacity-90 hover:scale-105 transform transition duration-500 hover:shadow-md"
-            data-aos="zoom-in-up"
-            data-aos-duration="1500"
-          >
-            <div>
-              <img className=" rounded-xl  " src={cardImg} alt="" />
-            </div>
-            <div>
-              <h2 className="pt-4  text-violet-500">
-                {" "}
-                E-Commerce Landing Page{" "}
-              </h2>
-              <p className="text-gray-700">
-                E-Commerce Landing Page Developed Using React.js, Tailwind Css{" "}
-              </p>
-              <button className="border-[1.5px] rounded-xl max-h-10 px-4 p-2 mt-4  text-white  font-600 bg-violet-500 border-solid border-violet-500  hover:bg-white hover:text-violet-500 hover:font-[500] ease-in duration-300">
-                View{" "}
-              </button>
-            </div>
-          </div>
+
           {/* 2nd card end */}
 
           {/* 3rd Card Start */}
-          <div
+          {/* <div
             className="cardshadow w-full p-4 bg-white border-solid border-2  lg:w-1/4 rounded-xl hover:opacity-90 hover:scale-105 transform transition duration-500 hover:shadow-md"
             data-aos="zoom-in-up"
             data-aos-duration="1500"
@@ -76,7 +91,7 @@ const Projects = () => {
                 View{" "}
               </button>
             </div>
-          </div>
+          </div> */}
           {/* 3rd Card end */}
         </div>
       </div>
