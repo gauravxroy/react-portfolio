a556ec7ca9f18b51965aee145e8284586c0a447a
Gaurav Roy <96660449+gauravxroy@users.noreply.github.com>
2023-10-05 21:25:12 +0530

    new about section added

--- src/components/UI/About.jsx
+++ src/components/UI/About.jsx
@@ -1,33 +1,66 @@
 import React from "react";
 import AvatarImg from "../Asset/Images/profile-pic.png";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
 const About = () => {
   return (
     <section id="about" className=" bg-[#111827] ">
+      <div className="mb-10 flex items-center justify-between flex-wrap container">
+        <div className="mb-7  sm:mb-0">
+          <h2 className="text-violet-500 font-[800] text-[2rem] mb-5 text-underline ">
+            About
+          </h2>
+        </div>
+      </div>
       <div
-        className="max-w-sm w-full lg:max-w-full lg:flex container"
+        className="max-w-sm w-full lg:max-w-full lg:flex container cursor-pointer"
         data-aos="zoom-in-up"
         data-aos-duration="1200"
       >
-        {/* <div
-          className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-          title="Woman holding a mug"
-        ></div> */}
-        <div className="border-solid border-2  lg:border-gray-400 bg-white rounded-xl p-4 flex flex-col justify-between leading-normal">
-          <div className="mb-8">
-            <p className="text-sm text-gray-600 flex items-center">
-              <svg
-                className="fill-current text-gray-500 w-3 h-3 mr-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
+        <div className=" lg:border-gray-400 bg-white rounded-3xl p-4 flex justify-between leading-normal">
+          <div
+            className="mb-8  mt-8 flex  gap-8  flex-wrap flex-col "
+            style={{ alignItems: "center" }}
+          >
+            <img
+              class=" w-32 h-32 rounded-full shadow-lg mx-auto flex"
+              src={AvatarImg}
+              alt="product designer"
+            />
+
+            {/* <div className="text-gray-900 font-bold text-xl ">Gaurav Roy</div> */}
+            <div className="">
+              <div className="text-sm text-center">
+                <p className="text-gray-900 font-bold text-xl leading-none">
+                  Gaurav Roy
+                </p>
+                <p className="text-gray-600">Software Developer</p>
+              </div>
+              <div
+                className="social flex items-center gap-2 justify-center mt-2"
+                style={{ color: "#7c3aed" }}
               >
-                <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-              </svg>
-              Members only
-            </p>
-            <div className="text-gray-900 font-bold text-xl mb-2">
-              Can coffee make you a better developer?
+                <span>
+                  <a href="/" className="hover:shadow-lg">
+                    {" "}
+                    <FaGithub size={20} />
+                  </a>
+                </span>
+                <span>
+                  <a href="/">
+                    {" "}
+                    <FaLinkedin size={20} />
+                  </a>
+                </span>
+                <span>
+                  <a href="/">
+                    {" "}
+                    <FaTwitter size={20} />
+                  </a>
+                </span>
+              </div>
             </div>
-            <p className="text-gray-700 text-base">
+            <p className="text-gray-700 text-base mx-1">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
               exercitationem praesentium nihil. Lorem ipsum dolor sit amet
@@ -37,17 +70,6 @@ const About = () => {
               temporibus autem, labore laborum doloremque iste debitis animi
               aspernatur!
             </p>
-          </div>
-          <div className="flex items-center">
-            <img
-              className="w-10 h-10 rounded-full mr-4"
-              src={AvatarImg}
-              alt="Avatar of Gaurav"
-            />
-            <div className="text-sm">
-              <p className="text-gray-900 leading-none">Gaurav Roy</p>
-              <p className="text-gray-600">Software Developer</p>
-            </div>
           </div>
         </div>
       </div>
